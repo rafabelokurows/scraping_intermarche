2762345fadecb14256f1a094b5637677ed4a5605
Rafael Belokurows <rafabelokurows@gmail.com>
2024-09-25 14:36:00 +0100

    Muitas coisas

--- new_scraping_intermarche.py
+++ new_scraping_intermarche.py
@@ -8,8 +8,10 @@ import os
 import time
 import random
 import urllib3
+#import ua_generator
 from collections import deque
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
+#from curl_cffi import requests
 
 # url = "https://www.loja-online.intermarche.pt/shelves/frutas-e-legumes/frutas/laranjas-e-outros-citrinos/11510"
 # url = "https://www.loja-online.intermarche.pt/shelves/frutas-e-legumes/frutas/macas-e-peras/11508"
@@ -52,172 +54,122 @@ def make_request(url, headers, payload, max_retries=3):
             if retries < max_retries:
                 print(f"Retrying ({retries}/{max_retries})...")
     raise Exception(f"Failed to make request to {url} after {max_retries} attempts")
+
+today = datetime.now().strftime("%Y%m%d")
+today_dir = os.path.join("./data/", today)
+today_hour = datetime.now().strftime("%Y%m%d%H%M")
+#%%
+
+#%%
+#### find it's the first execution for the day ####
+log_file = os.path.join(f"./log/",f"{today}.txt")
+print(f"Searching for log file {log_file}")
+if os.path.exists(log_file):
+    print(f"File '{today}.txt' exists in the folder log")
+    with open(log_file, 'r') as file:
+      last_exec = file.readline().strip()
+    last_exec = int(last_exec)
+    print(f"Last exec: {last_exec}")
+else:
+    last_exec = 0
+    print(f"First execution of the day")
 #%%
 
 #%%
 #### ENCONTRANDO CATEGORIAS ####
-starting_url = "https://www.loja-online.intermarche.pt/shelves/frutas-e-legumes/frutas/laranjas-e-outros-citrinos/11510"
-try:
-    response = make_request(starting_url, headers, payload)
-    # Process the response
-except Exception as e:
-    print(f"Final error: {e}")
-match = re.search(r'window\.__REACT_ESI__\[.*?\] = \{.*?"categories":(.*?)};\s*document\.currentScript\.remove\(\);', response.text, re.DOTALL)
+if last_exec == 0:
+    starting_url = "https://www.loja-online.intermarche.pt/shelves/frutas-e-legumes/frutas/laranjas-e-outros-citrinos/11510"
+    try:
+        response = make_request(starting_url, headers, payload)
+        # Process the response
+    except Exception as e:
+        print(f"Final error: {e}")
+    match = re.search(r'window\.__REACT_ESI__\[.*?\] = \{.*?"categories":(.*?)};\s*document\.currentScript\.remove\(\);', response.text, re.DOTALL)
 
-if match:
-    categories_json_str = match.group(1).strip()
-    
-    # Step 3: Convert the extracted string into a Python dictionary
-    categories_data = json.loads(categories_json_str)
-    
-    # Step 4: Flatten the category tree into a list of dictionaries
-    categories_list = []
+    if match:
+        categories_json_str = match.group(1).strip()
+        
+        # Step 3: Convert the extracted string into a Python dictionary
+        categories_data = json.loads(categories_json_str)
+        
+        # Step 4: Flatten the category tree into a list of dictionaries
+        categories_list = []
 
-    def extract_categories(tree):
-        for node in tree:
-            category_info = {
-                "level": node.get("level"),
-                "isPromo": node.get("isPromo"),
-                "id": node.get("id"),
-                "key": node.get("key"),
-                "title": node.get("title"),
-                "slug": node.get("slug"),
-                "link": node.get("link"),
-                "picto": node.get("picto"),
-                "hasAlcohol": node.get("hasAlcohol")
-            }
-            categories_list.append(category_info)
-            # Recur for children if any
-            if "children" in node:
-                extract_categories(node["children"])
+        def extract_categories(tree):
+            for node in tree:
+                category_info = {
+                    "level": node.get("level"),
+                    "isPromo": node.get("isPromo"),
+                    "id": node.get("id"),
+                    "key": node.get("key"),
+                    "title": node.get("title"),
+                    "slug": node.get("slug"),
+                    "link": node.get("link"),
+                    "picto": node.get("picto"),
+                    "hasAlcohol": node.get("hasAlcohol")
+                }
+                categories_list.append(category_info)
+                # Recur for children if any
+                if "children" in node:
+                    extract_categories(node["children"])
 
-    # Start extracting from the root of the category tree
-    extract_categories(categories_data["tree"])
+        # Start extracting from the root of the category tree
+        extract_categories(categories_data["tree"])
 
-    # Step 5: Create a DataFrame from the flattened list
-    df_categories = pd.DataFrame(categories_list)
+        # Step 5: Create a DataFrame from the flattened list
+        df_categories = pd.DataFrame(categories_list)
 
-levels = df_categories.query("level == 3").reset_index(drop=True)
-print(f'There are {levels.shape[0]} categories to scrape')
+    levels = df_categories.query("level == 3").reset_index(drop=True)
+    print(f'There are {levels.shape[0]} categories to scrape')
+    levels.to_csv(f'./data/{today}/categories.csv',encoding='utf-8-sig', index=False)
+else:
+    print("File 'df_categories_intermarche.csv' exists in the folder 'c:/Users/BELOKUROWSR/Desktop/inter/scraping_intermarche/'.")
+    df_categories = pd.read_csv(f'./data/{today}/categories.csv')
 #%%
 
 #%%
-# df_products = pd.DataFrame()
-# df_control = pd.DataFrame()
-# for index, row in levels.iterrows():
-#     aux_products = pd.DataFrame()
-#     if index % 10 == 0:
-#       print(f"Sleep, index: {index}")
-#       time.sleep(random.uniform(20, 60))
-#     url = f"https://www.loja-online.intermarche.pt{row['link']}"
-#     #print(url)
-#     #response = requests.request("GET", url, headers=headers, data=payload,verify=False)
-#     try:
-#         response = make_request(url, headers, payload)
-#     except Exception as e:
-#         print(f"Final error: {e}")
+# last_exec = 0
+# try:
+#     # Open the text file and read the first row
+#     with open("./log/last_execution_all.txt", 'r') as file:
+#       first_row = file.readline().strip()
 
-#     js_data_matches = re.findall(r'window\.__REACT_ESI__\[.*?\] = (\{.*?\});', response.text, re.DOTALL)
-#     filtered_matches = [item for item in js_data_matches if '"list":{"products"' in item]
+#     # Extract the number from the first row using a regex
+#     last_exec = re.findall(r'\d+\.?\d*', first_row)
 
-#     product_list = []
-#     print(f'Matches: {len(filtered_matches)}')
-#     if len(filtered_matches) == 0:
-#       print(js_data_matches)
-#     # Step 2: Iterate over each match and extract the products
-#     for js_data_str in filtered_matches:
-#         try:
-#             # Convert the extracted string to a Python dictionary
-#             js_data = json.loads(js_data_str)
-            
-#             # Access the products list
-#             products = js_data.get('list', {}).get('products', [])
-            
-#             # Step 3: Extract details from each product
-#             for product in products:
-#                 product_id = product.get('id')
-#                 title = product.get('informations', {}).get('title')
-#                 ean = product.get('ean')
-#                 family = product.get('famillyId')
-#                 department = product.get('departmentId')
-#                 packaging = product.get('informations', {}).get('packaging')
-#                 brand = product.get('informations', {}).get('brand')
-#                 category = product.get('informations', {}).get('category')
-#                 price = product.get('prices', {}).get('unitPrice', {}).get('concatenated')
-#                 price_per_unit = product.get('prices', {}).get('productPrice', {}).get('concatenated')
-#                 image_url = product.get('informations', {}).get('allImages', [{}])[0].get('src')
-#                 promo_end = product.get('informations', {}).get('highlight', {}).get('endDate')
-#                 #origin = product.get('informations', {}).get('originPlp')
-#                 product_url = product.get('url')
-                
-#                 product_list.append({
-#                     'Product ID': product_id,
-#                     'Title': title,
-#                     'EAN': ean,
-#                     'Family ID': family,
-#                     'Department ID': department,
-#                     'Packaging': packaging,
-#                     'Brand': brand,
-#                     'Category': category,
-#                     'Price': price,
-#                     'Price Per Unit': price_per_unit,
-#                     'Image URL': image_url,
-#                     'Promo End': promo_end,
-#                     #'Origin': origin,
-#                     'Product URL': product_url
-#                 })
+#     if last_exec:
+#         last_exec = int(last_exec[0])  # Convert to float if it's a decimal
+#     else:
+#         last_exec = 0  # Set to 0 if no number is found in the first row
 
-#         except json.JSONDecodeError:
-#             print("Error decoding JSON for a match.")
-    
-#     aux_products = pd.DataFrame(product_list)
-#     print(aux_products)
-#     aux_control = pd.DataFrame([{
-#                         'index': index,
-#                         'no_products': aux_products.shape[0],
-#                         'no_list_products': len(product_list),
-#                         'id': row['id'],
-#                         'title': row['title']
-    
-#                     }])
-#     df_products = pd.concat([df_products, aux_products])
-#     print(f"{row['title']}: {aux_products.shape[0]}") 
-#     df_control = pd.concat([df_control, aux_control])
-
-last_exec = 0
-try:
-    # Open the text file and read the first row
-    with open("./log/last_execution_all.txt", 'r') as file:
-      first_row = file.readline().strip()
-
-    # Extract the number from the first row using a regex
-    last_exec = re.findall(r'\d+\.?\d*', first_row)
-
-    if last_exec:
-        last_exec = int(last_exec[0])  # Convert to float if it's a decimal
-    else:
-        last_exec = 0  # Set to 0 if no number is found in the first row
-
-except FileNotFoundError:
-    # If the file doesn't exist, set number to 0
-    last_exec = 0
-
-last_exec = int(last_exec)
-print(f'Last exec: {last_exec}')
+# except FileNotFoundError:
+#     # If the file doesn't exist, set number to 0
+#     last_exec = 0
+#last_exec = int(last_exec)
+#print(f'Last exec: {last_exec}')
 #last_results = deque(maxlen=3)
+
+s = requests.Session()
+s.headers.update(headers)
 errors = 0
 df_products = pd.DataFrame()
 df_control = pd.DataFrame()
-for index, row in levels.iloc[last_exec:,].iterrows():
+#%%
+
+#%%
+for index, row in levels.iloc[last_exec:20,].iterrows():
     aux_products = pd.DataFrame()
     url = f"https://www.loja-online.intermarche.pt{row['link']}"
-    if index % 10 == 0 & index != 0:
-        print(f"Sleep, index: {index}")
-        #time.sleep(random.uniform(20, 60))
+    
     #print(url)
     #response = requests.request("GET", url, headers=headers, data=payload,verify=False)
+    #ua = ua_generator.generate(browser=('chrome', 'edge'))
+    #s.headers.update(ua.headers.get())
+    print(index)
+    print(s.headers)
+    # Making the POST request
     try:
-        response = make_request(url, headers, payload)
+        response = make_request(url, s.headers , payload)
         # Process the response
     except Exception as e:
         print(f"Final error: {e}")
@@ -260,7 +212,6 @@ for index, row in levels.iloc[last_exec:,].iterrows():
                 price_per_unit = product.get('prices', {}).get('productPrice', {}).get('concatenated')
                 image_url = product.get('informations', {}).get('allImages', [{}])[0].get('src')
                 promo_end = product.get('informations', {}).get('highlight', {}).get('endDate')
-                #origin = product.get('informations', {}).get('originPlp')
                 product_url = product.get('url')
                 
                 product_list.append({
@@ -276,21 +227,15 @@ for index, row in levels.iloc[last_exec:,].iterrows():
                     'Price Per Unit': price_per_unit,
                     'Image URL': image_url,
                     'Promo End': promo_end,
-                    #'Origin': origin,
                     'Product URL': product_url
                 })
 
-                
-
-                # Print or store the extracted data
-                # print(f"Product ID: {product_id}")
-                # print(f"Title: {title}")
-                # print(f"Price: {price}")
-                # print(f"Image URL: {image_url}")
-                # print(f"Origin: {origin}")
-                # print("-" * 40)
         except json.JSONDecodeError:
             print("Error decoding JSON for a match.")
+
+    if index % 10 == 0 & index != 0:
+        print(f"Sleep, index: {index}")
+        time.sleep(random.uniform(20, 60))
 
     aux_products = pd.DataFrame(product_list)
     aux_control = pd.DataFrame([{
@@ -300,29 +245,44 @@ for index, row in levels.iloc[last_exec:,].iterrows():
                     'title': row['title']
 
                 }])
-    #current_result = len(product_list)
-    #last_results.append(current_result)
     print(f"{row['title']} : {aux_products.shape[0]}") 
 
     df_products = pd.concat([df_products, aux_products])
     df_control = pd.concat([df_control, aux_control])
 
+s.close()
+#%%
+
+#%%
+new_control = levels.merge(df_control,how="left")
+if not last_exec:
+    last_exec = index
 last_exec = str(last_exec)
+
 if (index < levels.shape[0]):
-  with open('./log/last_execution_all.txt', 'w') as f:
-        f.write(last_exec)
+    print(f"The show must go on! Last exec: {last_exec}")
+    with open(os.path.join(f"./log/",f"{today}.txt"), 'w') as f:
+            f.write(last_exec)
 elif (index == levels.shape[0]):
-  with open('./log/last_execution_all.txt', 'w') as f:
-        f.write(levels.shape[0])
-today = datetime.now().strftime("%Y%m%d")
-today_dir = os.path.join("./data/", today)
-today_hour = datetime.now().strftime("%Y%m%d%H%s")
+    print(f"Finished for today! Last exec: {last_exec}")
+    with open(os.path.join(f"./log/",f"{today}.txt"), 'w') as f:
+            f.write(levels.shape[0])
+
+file_name_final = os.path.join(today_dir,f"{today}_all_products_intermarche.csv")
+if os.path.exists(file_name_final):
+    previous_df = pd.read_csv(file_name_final)
+    df_products = pd.concat([previous_df,df_products]).reset_index(drop=True)
+    print(f"File '{file_name_final}' exists in the folder: {df_products.shape[0]} products in total")
+else:
+    print(f"File '{file_name_final}' did not exist in the folder, creating new file: {df_products.shape[0]} products in total")
+
+
 print("Salvando ficheiros")
 os.makedirs(today_dir, exist_ok=True)
-filename_csv = os.path.join(today_dir,f"{today_hour}_all_products_intermarche.csv")
-filename_pkl = os.path.join(today_dir,f"{today_hour}_all_products_intermarche.pkl")
-filename_cat_csv = os.path.join(today_dir,f"{today_hour}_df_categories_intermarche.csv")
+filename_csv = os.path.join(today_dir,f"{today}_all_products_intermarche.csv")
+filename_pkl = os.path.join(today_dir,f"{today}_all_products_intermarche.pkl")
+filename_cat_csv = os.path.join(today_dir,'categories.csv')
 df_products.to_csv(filename_csv,encoding='utf-8-sig', index=False)
 df_products.to_pickle(filename_pkl)
-df_control.reset_index(drop=True).to_csv(filename_cat_csv, index=False,encoding='utf-8-sig')
+new_control.reset_index(drop=True).to_csv(filename_cat_csv, index=False,encoding='utf-8-sig')
 #%%
